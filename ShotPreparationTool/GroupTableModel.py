e5ac8c5b6aec440e6dcd14558067007c53782a22
Blake <blake.raymond@aquaticinformatics.com>
2014-01-18 00:59:14 -0800

    Add model classes for group list and group values with HDF5 backend

--- ShotPreparationTool/GroupTableModel.py
+++ ShotPreparationTool/GroupTableModel.py
@@ -34,11 +34,15 @@ class GroupTableModel(QtCore.QAbstractTableModel):
             if column == 0:
                 return self.__group().keys()[row]
             elif column == 1:
-                value = self.__group().values()[row][()]
-                if isinstance(value, np.ndarray):
-                    return str(list(value))
+                group = self.__group().values()[row]
+                if 'source_expression' in group.attrs:
+                    return group.attrs['source_expression']
                 else:
-                    return str(value)
+                    value = group[()]
+                    if isinstance(value, np.ndarray):
+                        return str(list(value))
+                    else:
+                        return str(value)
 
     def setData(self, index, value, role=None):
         if role == QtCore.Qt.EditRole:
@@ -60,10 +64,16 @@ class GroupTableModel(QtCore.QAbstractTableModel):
                     return False
                 try:
                     new_value = eval(new_value_string)
-                except:
-                    new_value = new_value_string
-                del self.h5file[current_name]
-                self.h5file[current_name] = new_value
+                except Exception as expt:
+                    message_box = QtGui.QMessageBox(None)
+                    message_box.warning(None, '', 'Could not evaluate expression: \"%s\"; error: \n\n%s' % (
+                    new_value_string, expt.message))
+                    return False
+                else:
+                    del self.h5file[current_name]
+                    self.h5file[current_name] = new_value
+                    self.h5file[current_name].attrs['source_expression'] = new_value_string
+
             self.dataChanged.emit(index, index)
             return True
         return False
@@ -84,20 +94,20 @@ if __name__ == '__main__':
     app = QtGui.QApplication([])
 
     if os.path.exists('foobar.h5'):
-        os.remove('foobar.h5')
-
-    h5file = h5py.File('foobar.h5')
-    groups = h5file.create_group('groups')
-    group1 = groups.create_group('group1')
-    group1['rb_pump_amplitude'] = 1
-    group1['rb_pump_detuning'] = 2
-    group1['Channels.Parameters.Mass.FirstMass'] = [14, 16, 18, 28, 32, 40, 44]
+        h5file = h5py.File('foobar.h5')
+    else:
+        h5file = h5py.File('foobar.h5')
+        groups = h5file.create_group('groups')
+        group1 = groups.create_group('group1')
+        group1['rb_pump_amplitude'] = 1
+        group1['rb_pump_detuning'] = 2
+        group1['Channels.Parameters.Mass.FirstMass'] = [14, 16, 18, 28, 32, 40, 44]
 
     model = GroupTableModel(h5file, group_name='groups/group1', parent=None)
-    model.insertRows(0, 1)
 
     view = QtGui.QTableView()
     view.setModel(model)
 
     view.show()
     app.exec_()
+    h5file.close()
