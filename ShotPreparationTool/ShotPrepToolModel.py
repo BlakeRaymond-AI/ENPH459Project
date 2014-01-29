2f994b92f2be2099502008e37cd97c4ccd5b9fbf
Jeffrey Burton <burton.jeff.r@gmail.com>
2014-01-28 22:31:56 -0800

    worked on discarding and saving data. They are currently not working. Looks like when writing to the workingFile, the actual h5 file is being destroyed. Might need a nicer interface to change data inside the GroupTableModels

---
+++ ShotPreparationTool/ShotPrepToolModel.py
@@ -0,0 +1,59 @@
+import os
+import os.path
+import shutil
+
+from PyQt4 import QtGui
+import h5py
+import GroupTableModel
+
+class ShotPrepToolModel(object):
+    def __init__(self, h5pathName):
+        self.h5pathName = h5pathName
+        if os.path.exists(self.h5pathName):
+            self.originalFile = h5py.File(self.h5pathName)
+        else:
+            self.originalFile = h5py.File(self.h5pathName)
+            self.originalFile.create_group('devices')
+
+        self.h5tempFileName = h5pathName + '.tmp'
+        if os.path.exists(self.h5tempFileName):
+            os.remove(self.h5tempFileName)
+        shutil.copy2(self.h5pathName, self.h5tempFileName)
+        self.workingFile = h5py.File(self.h5tempFileName)
+
+    def cleanUp(self):
+        self.originalFile.close()
+        self.workingFile.close()
+        os.remove(self.h5tempFileName)
+
+    def returnModelsInFile(self):
+        dict_of_devices = {}
+        for device in self.workingFile['devices']:
+            model = GroupTableModel.GroupTableModel(self.workingFile['devices'], device, parent=None)
+            dict_of_devices[device] = model
+        return dict_of_devices
+
+    def discardCharges(self):
+        #self.workingFile['devices'] = self.originalFile['devices']
+        del self.workingFile['devices']
+        self.originalFile.copy('devices', self.workingFile)
+
+    def saveChanges(self):
+        del self.originalFile['devices']
+        self.workingFile.copy('devices', self.originalFile)
+
+    def removeDevice(self, deviceName):
+        del self.workingFile[deviceName]
+
+    def addDevice(self, deviceName):
+        self.workingFile.create_group(deviceName)
+
+
+if __name__ == '__main__':
+
+    h5pathname = 'devices.h5'
+    test = ShotPrepToolModel(h5pathname)
+    devices = test.returnModelsInFile()
+
+    test.cleanUp()
+
