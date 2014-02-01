2623999883ad5263ef3ceb695f7098343eb3445c
Jeffrey Burton <burton.jeff.r@gmail.com>
2014-01-31 20:53:03 -0800

    Finished the basic outline of the runner tool model

---
+++ ShotRunnerTool/RunnerToolTableModel.py
@@ -0,0 +1,68 @@
+__author__ = 'Jeff'
+
+from PyQt4 import QtGui, QtCore
+
+class RunnerToolTableModel(QtCore.QAbstractTableModel):
+    def __init__(self, parent=None):
+        QtCore.QAbstractListModel.__init__(self, parent)
+        #want to load a json file and import the files stored there. Or create a new json file
+        self.numberOfRows = 1
+        self.testData = [('1', 'one'), ('2', 'two'), ('3', 'three')]
+
+    def rowCount(self, QModelIndex_parent=None, *args, **kwargs):
+        return len(self.testData)
+
+    def columnCount(self, QModelIndex_parent=None, *args, **kwargs):
+        return 2
+
+    def data(self, index, role=None):
+        row = index.row()
+        column = index.column()
+
+        if role == QtCore.Qt.DisplayRole or role == QtCore.Qt.EditRole:
+            name, data = self.testData[row]
+            if column == 0:
+                return name
+            if column == 1:
+                return data
+
+    def flags(self, index):
+        return QtCore.Qt.ItemIsEnabled | QtCore.Qt.ItemIsEditable | QtCore.Qt.ItemIsSelectable
+
+    def setData(self, index, value, role=None):
+        row = index.row()
+        column = index.column()
+        name, data = self.testData[row]
+        if role == QtCore.Qt.EditRole:
+            if column == 0:
+                name = value
+            elif column == 1:
+                data = value
+            self.testData[row] = (name, data)
+            self.dataChanged.emit(index, index)
+            return True
+        return False
+
+    def insertRows(self, position, rows, QModelIndex_parent=None, *args, **kwargs):
+        self.beginInsertRows(QtCore.QModelIndex(), 0, rows + self.numberOfRows)
+        for i in range(rows):
+            pass
+        self.endInsertRows()
+        return True
+
+    def addRow(self):
+        self.beginInsertRows(QtCore.QModelIndex(), 0, 0)
+        self.endInsertRows()
+        return True
+
+
+if __name__ == '__main__':
+    app = QtGui.QApplication([])
+
+    table_view = QtGui.QTableView()
+    table_model = RunnerToolTableModel()
+
+    table_view.setModel(RunnerToolTableModel())
+    table_view.show()
+
+    app.exec_()
