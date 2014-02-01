bb57f56a8f927e80f10d9f6336fb544724a206db
Jeffrey Burton <burton.jeff.r@gmail.com>
2014-02-01 15:54:58 -0800

    fixed errors caused by changing JsonUtils to static methods.

--- ShotRunnerTool/JsonUtils.py
+++ ShotRunnerTool/JsonUtils.py
@@ -4,59 +4,43 @@ import os
 class JsonUtils(object):
 
     @staticmethod
-    def newJsonFilePath(self, filename):
-        self.jsonPathName = filename
-        #if the file doesn't exit, create a new file
+    def newJsonFilePath(filename):
+        jsonPathName = filename
         if not os.path.exists(filename):
-            with open(filename, 'w') as self.jsonFile:
-                #todo: if the file exists open a dialog box asking if the user wants to overwrite it or not
-                self.jsonData = {}
-                return self.jsonPathName
+            with open(filename, 'w') as jsonFile:
+                return jsonPathName
         raise IOError, "The file %s already exists." %(filename)
 
     @staticmethod
-    def newJsonFile(self, filename):
-        self.jsonPathName = filename
-        #if the file doesn't exit, create a new file
+    def newJsonFile(filename):
+        jsonPathName = filename
         if not os.path.exists(filename):
-            with open(filename, 'w') as self.jsonFile:
-                #todo: if the file exists open a dialog box asking if the user wants to overwrite it or not
-                self.jsonData = {}
-                return self.jsonFile
+            with open(filename, 'w') as jsonFile:
+                return jsonFile
         raise IOError, "The file %s already exists." %(filename)
 
     @staticmethod
-    def getDataFromJsonFile(self, filename):
-        #load a json file into memory (update path and file)
-        #grab the file contents and add it into memory
-        self.jsonPathName = filename
-        #if the file exists open the file
+    def getDataFromJsonFile(filename):
+        jsonPathName = filename
         if os.path.exists(filename):
-            with open(filename, 'r') as self.jsonFile:
-                self.jsonData = json.loads(self.jsonFile.read())
-                return self.jsonData
+            with open(filename, 'r') as jsonFile:
+                jsonData = json.loads(jsonFile.read())
+                return jsonData
         raise IOError, "Couldn't import data from the file"
 
     @staticmethod
-    def saveJsonFile(self):
-        #save the json data to the json file
-        if self.jsonFile != None:
-            with open(self.jsonPathName, 'w') as self.jsonFile:
-                self.jsonFile.write(json.dumps(self.jsonData))
+    def saveJsonFileByPath(jsonPathName, jsonData):
+            with open(jsonPathName, 'w') as jsonFile:
+                jsonFile.write(json.dumps(jsonData))
 
     @staticmethod
-    def saveJsonFileAs(self, filename):
-        #save the data to another json file
-        with open(filename, 'w') as self.jsonFile:
-            self.jsonFile.write(json.dumps(self.jsonData))
+    def saveJsonFile(jsonFile, jsonData):
+        jsonFile.write(json.dumps(jsonData))
 
 def main():
-    test = JsonUtils()
-    test.openJsonFile('test.json')
-    print test.jsonData
-    test.jsonData['cruux'] = 'garply'
-    test.saveJsonFile()
-    test.saveJsonFileAs('test_save_as.json')
+    jsonDict = {'foo': 'bar'}
+    JsonUtils.saveJsonFileByPath('testStuff.json', jsonDict)
+    print JsonUtils.getDataFromJsonFile('testStuff.json')
 
 
 if __name__ == '__main__':
