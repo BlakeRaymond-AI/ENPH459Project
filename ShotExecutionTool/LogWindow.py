845c470efd6754215c64766ce68eca4782d23ab4
Blake <blake.raymond@aquaticinformatics.com>
2014-02-17 15:00:35 -0800

    Add LogWindow class to display subprocess output

---
+++ ShotExecutionTool/LogWindow.py
@@ -0,0 +1,14 @@
+__author__ = 'Blake'
+
+from PyQt4 import QtGui, QtCore
+
+
+class LogWindow(QtGui.QPlainTextEdit):
+    def __init__(self, parent):
+        QtGui.QPlainTextEdit.__init__(self, parent)
+
+    @QtCore.pyqtSlot(QtCore.QString)
+    def appendMessage(self, message):
+        self.appendPlainText(message)
+        self.verticalScrollBar().setValue(self.verticalScrollBar().maximum())
+
---
+++ ShotExecutionTool/LogWindowTests.py
@@ -0,0 +1,44 @@
+__author__ = 'Blake'
+
+import unittest
+
+from PyQt4 import QtGui, QtCore
+
+from signals.StringSignal import StringSignal
+from LogWindow import LogWindow
+
+
+class LogWindowTests(unittest.TestCase):
+    def setUp(self):
+        self.app = QtGui.QApplication([])
+
+    def test_canConnectSignalToAppendMessageSlot(self):
+        signal = StringSignal()
+        window = LogWindow(None)
+        signal.get().connect(window.appendMessage)
+        signal.get().emit('Foobar')
+        self.assertEqual('Foobar', window.toPlainText())
+
+    def test_canEmitMessageFromSeparateThread(self):
+        signal = StringSignal()
+        window = LogWindow(None)
+        signal.get().connect(window.appendMessage)
+
+        class EmitterThread(QtCore.QThread):
+            def __init__(self, signal, args):
+                QtCore.QThread.__init__(self)
+                self.signal = signal
+                self.args = args
+
+            def run(self):
+                self.signal.emit(*self.args)
+
+        thread = EmitterThread(signal.get(), ['Foobar'])
+        thread.run()
+        thread.wait()
+
+        self.assertEqual('Foobar', window.toPlainText())
+
+
+if __name__ == '__main__':
+    unittest.main()
