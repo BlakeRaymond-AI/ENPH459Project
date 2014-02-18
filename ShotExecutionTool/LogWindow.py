2f6534e02b16c3c6c6d68ea7e6111f85755e809e
Blake <blake.raymond@aquaticinformatics.com>
2014-02-17 17:19:51 -0800

    Make LogWindowController run() synchronous for testing, add async version for production
    Asynchronous call seems to only work inside an app's exec_ loop

--- ShotExecutionTool/LogWindow.py
+++ ShotExecutionTool/LogWindow.py
@@ -2,13 +2,13 @@ __author__ = 'Blake'
 
 from PyQt4 import QtGui, QtCore
 
-
 class LogWindow(QtGui.QPlainTextEdit):
     def __init__(self, parent):
         QtGui.QPlainTextEdit.__init__(self, parent)
 
     @QtCore.pyqtSlot(QtCore.QString)
     def appendMessage(self, message):
-        self.appendPlainText(str(message).strip())
+        formattedMessage = str(message).strip()
+        self.appendPlainText(formattedMessage)
         self.verticalScrollBar().setValue(self.verticalScrollBar().maximum())
 
--- ShotExecutionTool/LogWindowController.py
+++ ShotExecutionTool/LogWindowController.py
@@ -12,6 +12,15 @@ class LogWindowController(object):
         self.signal = StringSignal()
         self.signal.get().connect(self.logWindow.appendMessage)
 
+    def runAsync(self):
+        if not self.runner.isRunning():
+            self.runner.executeAsync()
+
+        self.outputEmitter = GeneratorEmitter(self.runner.outputStream(), self.signal.get())
+        self.errorEmitter = GeneratorEmitter(self.runner.errorStream(), self.signal.get())
+        self.outputEmitter.start()
+        self.errorEmitter.start()
+
     def run(self):
         if not self.runner.isRunning():
             self.runner.executeAsync()
@@ -23,8 +32,24 @@ class LogWindowController(object):
 
     def join(self):
         output, error = self.runner.join()
-        self.signal.get().emit(output)
-        self.signal.get().emit(error)
+        if output:
+            self.signal.get().emit(output)
+        if error:
+            self.signal.get().emit(error)
         self.outputEmitter.wait()
         self.errorEmitter.wait()
 
+
+from ScriptRunner import ScriptRunner
+from LogWindow import LogWindow
+from PyQt4 import QtGui
+
+if __name__ == '__main__':
+    app = QtGui.QApplication([])
+    runner = ScriptRunner('sample.py')
+    window = LogWindow(None)
+    controller = LogWindowController(runner, window)
+    window.show()
+    controller.runAsync()
+    app.exec_()
+    controller.join()
