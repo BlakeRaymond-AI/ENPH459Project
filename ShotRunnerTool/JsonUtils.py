6a9e058efd6ab3945388903a29138d048f0f1f89
Jeffrey Burton <burton.jeff.r@gmail.com>
2014-03-02 12:46:19 -0800

    Removed extra unused functions from the JsonUtils class.

--- ShotRunnerTool/JsonUtils.py
+++ ShotRunnerTool/JsonUtils.py
@@ -2,44 +2,22 @@ import json
 import os
 
 class JsonUtils(object):
-
-    @staticmethod
-    def newJsonFilePath(filename):
-        jsonPathName = filename
-        if not os.path.exists(filename):
-            with open(filename, 'w') as jsonFile:
-                return jsonPathName
-        raise IOError, "The file %s already exists." %(filename)
-
-    @staticmethod
-    def newJsonFile(filename):
-        jsonPathName = filename
-        if not os.path.exists(filename):
-            with open(filename, 'w') as jsonFile:
-                return jsonFile
-        raise IOError, "The file %s already exists." %(filename)
-
     @staticmethod
     def getDataFromJsonFile(filename):
         if os.path.exists(filename):
             with open(filename, 'r') as jsonFile:
                 jsonData = json.loads(jsonFile.read())
                 return jsonData
-        raise IOError, "Couldn't import data from the file"
+        raise IOError, "Couldn't import data from the file '%s'" %(filename)
 
     @staticmethod
     def saveJsonFileByPath(jsonPathName, jsonData):
             with open(jsonPathName, 'w') as jsonFile:
                 jsonFile.write(json.dumps(jsonData))
 
-    @staticmethod
-    def saveJsonFile(jsonFile, jsonData):
-        jsonFile.write(json.dumps(jsonData))
 
 def main():
-    jsonDict = {'foo': 'bar'}
-    print JsonUtils.getDataFromJsonFile('test.json')
-
+    JsonUtils.getDataFromJsonFile('test io error.json')
 
 if __name__ == '__main__':
     main()
