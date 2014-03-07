0c51e8bd5a2a735517ec55d27fd89e6b98b1756b
Jeffrey Burton <burton.jeff.r@gmail.com>
2014-03-06 19:19:26 -0800

    Added unit tests for the ShotRunnerToolTableModel.

---
+++ ShotRunnerTool/testShotRunnerToolTableModel.py
@@ -0,0 +1,52 @@
+__author__ = 'Jeff'
+
+import os
+from unittest import TestCase
+from ShotRunnerToolTableModel import *
+
+class TestShotRunnerToolTableModel(TestCase):
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    def test_rowCount(self):
+        tableModel = ShotRunnerToolTableModel()
+        self.assertEqual(tableModel.rowCount(), 1) #check that there is one row by default
+        tableModel.addRow()
+        tableModel.fileData[1]['scriptFileName'] = 'test'
+        self.assertEqual(tableModel.rowCount(), 2) #make sure that there are now 2 rows
+
+    def test_columnCount(self):
+        tableModel = ShotRunnerToolTableModel()
+        self.assertEqual(tableModel.columnCount(), 2)
+
+    def test_addRow(self):
+        tableModel = ShotRunnerToolTableModel()
+        self.assertEqual(tableModel.rowCount(), 1) #check that there is one row by default
+        tableModel.addRow()
+        self.assertEqual(tableModel.rowCount(), 2) #check that the number of rows has been incremented
+
+    def test_saveDataToFileByPath(self):
+        tableModel = ShotRunnerToolTableModel()
+        filePath = 'test_TableModelSaveData.json'
+        tableModel.saveDataToFileByPath(filePath)
+        self.assertTrue(os.path.exists(filePath))
+        os.remove(filePath)
+
+    def test_close(self):
+        tableModel = ShotRunnerToolTableModel()
+        tableModel.addRow()
+        tableModel.addRow()
+        tableModel.close()
+        self.assertEqual(tableModel.rowCount(), 1)
+
+    def test_getScriptsAndSettingsFilePaths(self):
+        tableModel = ShotRunnerToolTableModel()
+        tableModel.addRow()
+        tableModel.fileData[1]['scriptFilePath'] = 'foo'
+        tableModel.fileData[1]['settingsFilePath'] = 'baz'
+        settings, scripts = tableModel.getScriptsAndSettingsFilePaths()
+        self.assertEqual(settings[1], 'foo')
+        self.assertEqual(scripts[1], 'baz')
