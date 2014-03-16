7a78fe1a18c71caf19d03e55a8143812e9bc4a24
Blake <blake.raymond@aquaticinformatics.com>
2014-03-16 03:17:56 -0700

    Change the prep tool working file to be memory-only

--- ShotPreparationTool/ShotPrepToolModel.py
+++ ShotPreparationTool/ShotPrepToolModel.py
@@ -1,6 +1,5 @@
 import os
 import os.path
-import shutil
 
 import h5py
 
@@ -22,16 +21,13 @@ class ShotPrepToolModel(object):
             self.originalFile.flush()
 
         self.h5tempFileName = h5pathName + '.tmp'
-        if os.path.exists(self.h5tempFileName):
-            raise RuntimeError('The file \"%s\" appears to be in use by another instance of the tool.' % h5pathName)
-        shutil.copy2(self.h5pathName, self.h5tempFileName)
-        self.workingFile = h5py.File(self.h5tempFileName)
+        self.workingFile = h5py.File(self.h5tempFileName, driver='core', backing_store=False)  # memory-only
+        self.originalFile.copy(DEVICES_GROUP_NAME, self.workingFile)
         self.__buildModelsInFile()
 
     def cleanUp(self):
         self.originalFile.close()
         self.workingFile.close()
-        os.remove(self.h5tempFileName)
 
     def __buildModelsInFile(self):
         self.dict_of_devices = {}
@@ -68,5 +64,5 @@ class ShotPrepToolModel(object):
         self.__buildModelsInFile()
 
     def saveAs(self, filename):
-        self.workingFile.flush()
-        shutil.copy2(self.h5tempFileName, filename)
+        newFile = h5py.File(filename)
+        self.workingFile.copy(DEVICES_GROUP_NAME, newFile)
