cf6e2725b0b759ff738013ac1e330b6146a4ff88
Jeffrey Burton <burton.jeff.r@gmail.com>
2014-03-20 21:20:23 -0700

    Finished the script to convert from a py settings file to an h5 settings file.

--- SettingsConverterScript/settingsConverter.py
+++ SettingsConverterScript/settingsConverter.py
@@ -1,24 +1,18 @@
 import settingsTemplate
 import h5py
 
-"""
-create an h5 file
-read all elements from the settings template
-filter out all non-custom dicts
-make a list of all remaining objects in the file
-iterate thru the list:
-for each device in the list create a dict in the h5 file
-fill that dict with the default settings from the read file
-"""
 
 h5file = h5py.File('default_name.h5','w')
 deviceNames = filter(lambda x: type(getattr(settingsTemplate,x)) == dict and x != '__builtins__', dir(settingsTemplate))
-
+deviceGroup = h5file.create_group('devices')
 for deviceString in deviceNames:
     device = getattr(settingsTemplate, deviceString)
-    h5device = h5file.create_group(deviceString)
+    h5device = deviceGroup.create_group(deviceString)
     for key in device.keys():
         h5device[key] = device[key]
-        
+        h5device[key].attrs['source_expression'] = str(device[key])
+
+        #raise Exception, "Error importing data for device %s, data %s" %(deviceString, key)
+        #print "Error importing data for device %s, data %s" %(deviceString, key)
 h5file.close()
 
